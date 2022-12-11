ddea5c2c16d786ee5fe2f6ade04b6ab2c4ef9b82
Quốc Bảo <81245741+Pheodrys-QB@users.noreply.github.com>
2022-12-11 10:35:42 +0700

    Getting started

---
+++ app.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import hbs_sections from 'express-handlebars-sections'
+
+
+import numeral from 'numeral';
+
+const app = express();
+
+app.use('/public', express.static('public'));
+
+app.engine('hbs', engine({
+  // defaultLayout: 'main.hbs'
+  extname: 'hbs',
+  defaultLayout: 'main',
+  helpers: {
+    section: hbs_sections(),
+    format_number(val) {
+      return numeral(val).format('0,0');
+    }
+  }
+}));
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+// app.use(async function (req, res, next) {
+//   res.locals.lcCategories = await categoryService.findAllWithDetails();
+//   next();
+// });
+
+app.get('/', function (req, res) {
+  // res.send('Hello World.');
+  res.render('home');
+});
+
+app.get('/about', function (req, res) {
+  res.render('about');
+})
+
+app.use(function (req, res, next) {
+  res.render('404', { layout: false });
+});
+
+app.use(function (err, req, res, next) {
+  // console.error(err.stack);
+  res.status(500).render('500', {
+    stack: err.stack,
+    layout: false
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, function () {
+  console.log(`E-commerce application listening at http://localhost:${PORT}`);
+})
---
+++ utils/db.js
@@ -0,0 +1,15 @@
+import knexObj from 'knex';
+
+const knex = knexObj({
+  client: 'mysql2',
+  connection: {
+    host: '127.0.0.1',
+    port: 8889,
+    user: 'root',
+    password: 'root',
+    database: 'ecdb'
+  },
+  pool: { min: 0, max: 10 }
+});
+
+export default knex;
