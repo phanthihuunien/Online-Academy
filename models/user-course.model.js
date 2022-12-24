908cbcc0c772da83139fe614d83523db88f27cac
lienvanduc1808 <20127467@student.hcmus.edu.vn>
2022-12-24 11:43:44 +0700

    myCourse

--- models/user-course.model.js
+++ models/user-course.model.js
@@ -1,12 +1,14 @@
 import db from '../utils/db.js';
 export default{
-    getFeedbackWithCourseID(id){
-        const list =  db('user_course').where('ID_COURSE',id);
-
-
+    async getFeedbackWithCourseID(id){
+        const list =  await db('user_course').where('ID_COURSE',id);
         return list;
 
 
-    }
+    },
+    async findAllbyUserID(id){
+        const list = await db('user_course').where('ID_USER',id);
+        return list;
+    },
 
 }
