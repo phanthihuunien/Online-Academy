4eaa3ae00f0bbc9fb2b809b3ceebd7755ddc9dca
Quốc Bảo <81245741+Pheodrys-QB@users.noreply.github.com>
2023-01-05 09:29:13 +0700

    Update category.sql

--- category.sql
+++ category.sql
@@ -1,4 +1,7 @@
-INSERT INTO oadb.category (ID_CATE, CATENAME) VALUES (1, 'Web Development');
-INSERT INTO oadb.category (ID_CATE, CATENAME) VALUES (2, 'Game Development');
-INSERT INTO oadb.category (ID_CATE, CATENAME) VALUES (3, 'Mobile Development');
-INSERT INTO oadb.category (ID_CATE, CATENAME) VALUES (4, 'Data Science');
+INSERT INTO oadb.field (ID_CATE, CATENAME) VALUES ('IT');
+INSERT INTO oadb.field (ID_CATE, CATENAME) VALUES ('Economic');
+
+INSERT INTO oadb.category (ID_CATE, CATENAME) VALUES ('Web Development', 1);
+INSERT INTO oadb.category (ID_CATE, CATENAME) VALUES ('Game Development', 1);
+INSERT INTO oadb.category (ID_CATE, CATENAME) VALUES ('Mobile Development', 1);
+INSERT INTO oadb.category (ID_CATE, CATENAME) VALUES ('Data Science', 1);
