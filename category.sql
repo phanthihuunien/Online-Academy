22f34edf94718cb5f30ee7ef5c2d8889e4025d81
Quốc Bảo <81245741+Pheodrys-QB@users.noreply.github.com>
2023-01-09 21:44:42 +0700

    empty out

--- category.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO oadb.field (FIELDNAME) VALUES ('IT');
-INSERT INTO oadb.field (FIELDNAME) VALUES ('Economic');
-
-INSERT INTO oadb.category (CATENAME,ID_FIELD) VALUES ('Web Development', 1);
-INSERT INTO oadb.category (CATENAME,ID_FIELD) VALUES ('Game Development', 1);
-INSERT INTO oadb.category (CATENAME,ID_FIELD) VALUES ('Mobile Development', 1);
-INSERT INTO oadb.category (CATENAME, ID_FIELD) VALUES ('Data Science', 1);
